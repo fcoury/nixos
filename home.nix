6c17ec1ad2401e69b79d703de32864f0d877e91b
Felipe Coury <felipe.coury@gmail.com>
2024-01-09 22:53:59 -0500

    rust and lsp-config

--- home.nix
+++ home.nix
@@ -69,6 +69,9 @@ in
       nvim-tree-lua
       vim-visual-multi
       todo-comments-nvim
+      rust-tools-nvim
+      vim-cool
+      fidget-nvim
 
       (fromGitHub "e7868b38f402be94e859d479002df1418bc1e954" "main" "coffebar/neovim-project")
       (fromGitHub "68dde355a4304d83b40cf073f53915604bdd8e70" "master" "Shatur/neovim-session-manager")
